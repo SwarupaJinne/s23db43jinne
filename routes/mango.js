812457303622e3705ef7f8f52add0ce45cf66a13
SwarupaJinne <jinneswarupafall2022@gmail.com>
2023-04-27 21:19:27 -0500

    changed

--- routes/mango.js
+++ routes/mango.js
@@ -1,16 +1,27 @@
 var express = require('express');
 const mango_controlers= require('../controllers/mango');
 var router = express.Router();
-/* GET mango */
-// GET request for one mango.
-router.get('/', mango_controlers.mango_view_all_Page);
-/* GET detail mango page */
-router.get('/detail', mango_controlers.mango_view_one_Page);
-/* GET create mango page */
-router.get('/create', mango_controlers.mango_create_Page);
-module.exports = router;
-/* GET create update page */
-router.get('/update', mango_controlers.mango_update_Page);
-/* GET delete mango page */
-router.get('/delete', mango_controlers.mango_delete_Page);
 
+/* GET home page. */
+router.get('/', mango_controlers.mango_view_all_Page );
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}   
+/* GET create update page */
+router.get('/update',secured, mango_controlers.mango_update_Page);
+/* GET detail mango page */
+router.get('/detail',mango_controlers.mango_view_one_Page);
+
+/* GET create mango page */
+router.get('/create',secured, mango_controlers.mango_create_Page);
+
+
+/* GET delete mango page */
+router.get('/delete',secured, mango_controlers.mango_delete_Page);
+
+module.exports = router;
