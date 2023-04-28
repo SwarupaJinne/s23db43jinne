e00966a7fbe3e83cc203a6760889b41bad386336
SwarupaJinne <jinneswarupafall2022@gmail.com>
2023-04-27 21:32:05 -0500

    changed

--- models/mango.js
+++ models/mango.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 const mangoSchema = mongoose.Schema({
-    mango_type: String,
-    mango_quantity: String,
-    Mango_Price: Number
+    
+    mango_type: {type:String, minlength:2,maxlength:20},
+    mango_quantity: {type:String, minlength:5,maxlength:1000},
+    Mango_Price: {type:Number, min:10,max:2000}
 })
 module.exports = mongoose.model("mango",mangoSchema)
 
